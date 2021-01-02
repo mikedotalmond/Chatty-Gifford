8734ae35ebfbb89f62e3f5b95bf9668da24ca42b
mike <mikedotalmond@googlemail.com>
2021-01-02 00:20:30 +0000

    readme and some more comments

--- README.md
+++ README.md
@@ -4,5 +4,26 @@ Display a wall of GIFs driven by a simple Twitch live chat sentiment analysis an
 
 [Example](https://www.twitch.tv/videos/856384544)
 
-### Setup and configure
- * TODO
+
+### Getting started
+* Download or clone the repo. 
+* In `/dist/` rename `config.default.js` to `config.js` and follow the instructions there to finish setting things up. 
+* Open dist/index.html in a browser or assign as a browser-source in OBS etc.
+
+For faster config changes (or, for example, to run multiple different configs in different windows) any of the config key:value pairs can be overridden via the querystring, like...
+* index.html?channel=myFavouriteStreamer&giphy_rating=pg13
+
+
+### Build from source
+* `npm install`
+* `npm run build` or `npm run build-prod`
+
+
+### Uses
+* [tmi](https://www.npmjs.com/package/tmi)
+* [compromise](https://www.npmjs.com/package/compromise)
+* [afinn-165](https://www.npmjs.com/package/afinn-165)
+* [masonry-layout](https://www.npmjs.com/package/masonry-layout)
+* [timers-browserify](https://www.npmjs.com/package/timers-browserify)
+* [webpack](https://www.npmjs.com/package/webpack)
+* [webpack-cli](https://www.npmjs.com/package/webpack-cli)
--- dist/config.default.js
+++ dist/config.default.js
@@ -1,14 +1,11 @@
 /**
- * General config
+ * Make a copy of this file and rename it to `config.js`
  * 
- * 
- * Make a copy of this file and rename it to "config.js",
- * 
- * ... then replace the giphy.apiKey value with your own giphy API key 
+ * Then replace the giphy_apiKey value with your own giphy API key (signup is free)
  * - https://support.giphy.com/hc/en-us/articles/360020283431-Request-A-GIPHY-API-Key
  * - https://developers.giphy.com/
  * 
- * ... set the `channel` value to the twitch channel you want to use
+ * * Set `channel` value to the twitch channel you want to use
  */
 
 window.sentimentWall = {
@@ -19,19 +16,17 @@ window.sentimentWall = {
 
     // the twitch channel to observe...
     // can be anyone, twitch chats are all public and require no authentication to read from.
-    channel: 'mikedotalmond', // 1030jh, limmy, zafarcakes, etc...
+    channel: 'mikedotalmond', // 1030jh, coolcaits, limmy, pinkhexagon, sheila, toastie, zafarcakes, etc...
 
-    // number of gif items to keep in the display at once. will need to change if the grid size is altered in the CSS.
+    // Number of gif items to keep in the display at once. 
+    // Will need to change if the grid size is altered in the CSS.
     bufferSize: 25,
 
-
-    // giphy specifics
-
-    // 'g', 'pg13', or 'r' - keeping results clean, or not. pg13 seems to be generally ok for Twitch's ToS.
+    // giphy search settings
+    // 'g', 'pg13', or 'r' - keeping results clean, or not. pg13 *seems* to be generally ok for the Twitch ToS.
     // see: https://developers.giphy.com/docs/optional-settings#rating
     giphy_rating: "g",
-
     // put your giphy api key here - but DON'T then commit this file to version control
-    giphy_apiKey: "your-giphy-apikey", 
+    giphy_apiKey: "your-giphy-apikey",
   }
 };
